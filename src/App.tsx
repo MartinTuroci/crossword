fea69c8dd4c4cf01d8f5c73281e2b14fec518010
Martin Turoci <martinturoci@gmail.com>
2020-04-12 00:06:18 +0200

    feat: Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Board from './components/Board';
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <div className="app-container">
         <Board />
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      This crossword was originally published at Plus 7 Dní magazine.
+    </footer>
+  );
+};
+export default Footer;
