7b2da950fe5ddd6130ca49726790ed48778bb42f
Martin Turoci <martinturoci@gmail.com>
2020-04-11 23:54:07 +0200

    feat: json crossword

---
+++ src/util/crossword.ts
@@ -0,0 +1,1124 @@
+import { Tile, TileType } from '../model/tile.model';
+
+export const crossword: Tile[] = [
+  {
+    text: ['autor výroku: W. Blake'],
+    type: TileType.HINT,
+  },
+  {
+    text: ['písmeno X foneticky'],
+    type: TileType.HINT,
+  },
+  {
+    text: ['1.časť tajničky'],
+    type: TileType.HINT,
+  },
+  {
+    text: ['mesačné more'],
+    type: TileType.HINT,
+  },
+  {
+    text: ['patriaci ujkovi'],
+    type: TileType.HINT,
+  },
+  {
+    text: ['spod'],
+    type: TileType.HINT,
+  },
+  {
+    text: [''],
+    type: TileType.HINT,
+  },
+  {
+    text: ['svokor po česky'],
+    type: TileType.HINT,
+  },
+  {
+    text: ['americká poisťovacia skupina'],
+    type: TileType.HINT,
+  },
+  {
+    text: ['EČV Komárna'],
+    type: TileType.HINT,
+  },
+  {
+    text: ['zápasnícka žinienka'],
+    type: TileType.HINT,
+  },
+  {
+    text: ['elektrický výboj'],
+    type: TileType.HINT,
+  },
+  {
+    text: ['ekavské nárečie'],
+    type: TileType.HINT,
+  },
+  {
+    text: ['existovala'],
+    type: TileType.HINT,
+  },
+  {
+    text: ['zvyčajné zakončenie názvu filoz. smeru'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['takisto'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['obuvnícky nôž'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['česká hudobná skupina', 'fotografický archív'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['zdobená kamenná rakva'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['zhotovoval tkaninu', 'popevok'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: [''],
+    type: TileType.HINT,
+  },
+  {
+    text: ['kríženec leoparda s levicou', 'africká gazela'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['kukla', 'meno Lýdie'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['predložka'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: ['hľa po rusky', 'TV postava s dáždnikom'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['časť Bratislavy', 'citoslovce bedákania'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['násypný komín'],
+    type: TileType.HINT,
+  },
+  {
+    text: ['konár, vetva po nemecky'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['získali dobývaním', 'valí sa'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['je, po anglicky', 'Inštitút pre informatiku a štatistiku'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['stiahnutie (odb.)'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['slovom, skrátka (čast.)', 'metropola Litvy'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['odlišný', 'poplatok za stolovanie'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['klasická indická medicína'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['takmer (zastar.)', 'meno Timotey'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['český zápor'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: ['výklenok v stene', 'Video Music Awards'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['sopečná hornina', 'podraďovacia spojka'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['zvyšuje tlak'],
+    type: TileType.HINT,
+  },
+  {
+    text: ['slaná tyčinka'],
+    type: TileType.HINT,
+  },
+  {
+    text: [''],
+    type: TileType.HINT,
+  },
+  {
+    text: ['plakala (básn.)'],
+    type: TileType.HINT,
+  },
+  {
+    text: ['honosný dom', 'vyrobil liatím'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['2. časť tajničky', 'knieža Monaka'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: ['priesvit (lek.)'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['povolanie', '3. časť tajničky'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['výtvory klasikov'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['marocká jednotka hmostnosti', 'oslovenie pána v Ázii'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['druh leňocha'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['zubné zlato', 'glamorová pena'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['vziať, po anglicky', 'cín (zn.)'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['súlad (kniž.)'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['goniometrická funkcia', 'kto nie je Žid'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['citoslovce trúbenia', 'španielske mužské meno'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['patriaci Ale'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['meno herečky Mórovej', 'kašovité jedlo, čír.'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['juliánsky kalendár', 'boxerské zápasisko'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: [''],
+    type: TileType.HINT,
+  },
+  {
+    text: ['stredo - slovenský (skr.)'],
+    type: TileType.HINT,
+  },
+  {
+    text: ['nestranník', 'TV spoločnosť Taiwanu'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['ohraničená plocha', 'syn Kálefov'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['miesto pre štátny symbol'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['vzorec jodidu iridného', 'tepová frekvencia (skr.)'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['dvojhláska'],
+    type: TileType.HINT,
+  },
+  {
+    text: ['Lesotho (kód)'],
+    type: TileType.HINT,
+  },
+  {
+    text: ['zotriem (hovor.)'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: ['sedel na čele kráľovstva'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: ['vodcovia (z angl.)'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.RESULT,
+  },
+  {
+    text: ['hudba, po latinsky'],
+    type: TileType.HINT,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+  {
+    text: '',
+    type: TileType.WRITEABLE,
+  },
+];
